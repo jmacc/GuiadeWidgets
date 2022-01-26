28718457bdea3ed36c2a28536ae957d95a417305
jmacc <j2macc@gmail.com>
2022-01-26 14:46:08 -0600

    Botones onclick

--- app/src/main/java/com/example/guiadewidgets/BotonesActivity.kt
+++ app/src/main/java/com/example/guiadewidgets/BotonesActivity.kt
@@ -2,10 +2,22 @@ package com.example.guiadewidgets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class BotonesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_botones)
+    }
+
+    fun mostrar(widget: android.view.View) {
+        var mensaje = ""
+        var tag = widget.tag
+
+        when(tag){
+            "saludar"->{ mensaje = "Presionaste boton normal"}
+            "email"->{ mensaje = "Presionaste el image button"}
+        }
+        Toast.makeText(applicationContext,mensaje,Toast.LENGTH_LONG).show()
     }
 }
