84b47d5ba0d1e7b906bc835427c295275e1feb97
jmacc <j2macc@gmail.com>
2022-02-26 16:43:10 -0600

    Se agrego checkbos y Switch

--- app/src/main/java/com/example/guiadewidgets/SeleccionActivity.kt
+++ app/src/main/java/com/example/guiadewidgets/SeleccionActivity.kt
@@ -2,10 +2,32 @@ package com.example.guiadewidgets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.Switch
+import android.widget.Toast
 
 class SeleccionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seleccion)
     }
+
+	fun validaSeleccion(view: View) {
+		if(view is CheckBox){
+			if (view.isChecked){
+				Toast.makeText(applicationContext,"Esta realizando un checbox",Toast.LENGTH_LONG).show()
+			}else{
+				Toast.makeText(applicationContext,"No este seleccionado el checbox",Toast.LENGTH_LONG).show()
+			}
+
+		}else if(view is Switch){
+			if(view.isChecked){
+				Toast.makeText(applicationContext,"Esta realizando un Switch",Toast.LENGTH_LONG).show()
+			}else{
+				Toast.makeText(applicationContext,"No este seleccionado el checbox",Toast.LENGTH_LONG).show()
+			}
+		}
+
+	}
 }
